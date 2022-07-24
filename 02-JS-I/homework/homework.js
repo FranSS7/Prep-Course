630b180498a7e86fd1d6386beff8b963e659897e
FranSS7 <franciscosantillansuarez@hotmail.com>
2022-07-24 15:41:49 -0300

    Fran

--- 02-JS-I/homework/homework.js
+++ 02-JS-I/homework/homework.js
@@ -77,6 +77,7 @@ function menosQueNoventa(num) {
   // Devuelve "true" si el argumento de la función "num" es menor que noventa
   // De lo contrario, devuelve "false"
   // Tu código:
+  return num < 90
   
 }
 
@@ -84,6 +85,7 @@ function mayorQueCincuenta(num) {
   // Devuelve "true" si el argumento de la función "num" es mayor que cincuenta
   // De lo contrario, devuelve "false"
   // Tu código:
+  return num > 50
   
 }
 
